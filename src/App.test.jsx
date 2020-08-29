bfb2f1d65fcdfa986b54bfd455457500beb1e6a0
Aman <devproddeploy@gmail.com>
2020-08-29 21:01:22 +0530

    testing grahql queries with MockedProvider

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,3 +1,54 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react';
+import { fetchReposQuery } from './graphql-queries'
+import Repos from './components/Repos';
+import { MockedProvider } from '@apollo/client/testing';
+
+const mocks = [
+    {
+        request: {
+            query: fetchReposQuery
+        },
+        result: {
+            "data": {
+                "viewer": {
+                    "repositories": {
+                        "nodes": [
+                            {
+                                "name": "git-Book-3.0",
+                                "description": "Github GraphQL API",
+                                "url": "https://github.com/inblack67/git-Book-3.0",
+                                "languages": {
+                                    "nodes": [
+                                        {
+                                            "color": "#e34c26",
+                                            "name": "HTML"
+                                        },
+                                        {
+                                            "color": "#f1e05a",
+                                            "name": "JavaScript"
+                                        }
+                                    ]
+                                }
+                            }
+                        ]
+                    }
+                }
+            }
+        }
+    }
+];
+
+it('renders repos', async () => {
+    render(
+        <MockedProvider mocks={mocks} addTypename={false}>
+            <Repos />
+        </MockedProvider>
+    );
+    
+    expect(screen.getByText('Loading...')).toBeInTheDocument();
+
+    // getByText is not async
+    const repoTitle = await screen.findByText('Headphones');
+    expect(repoTitle).toBeInTheDocument();
+})
--- src/components/Repos.jsx
+++ src/components/Repos.jsx
@@ -4,16 +4,27 @@ import { useQuery } from '@apollo/client';
 
 const Repos = () => {
 
-    const { data, loading } = useQuery(fetchReposQuery);
+    const { data, loading, error } = useQuery(fetchReposQuery);
 
-    if(loading){
+    if (loading) {
         return <p className="flow-text center">Loading...</p>
     }
 
+    if (error) {
+        return <p className="flow-text center red-text">Error...</p>
+    }
+
     return (
-        <pre>
-            {JSON.stringify(data, null, 3)}
-        </pre>
+        <div>
+            <ul>
+                {data.viewer.repositories.nodes.map(repo => <li repo={repo} key={repo.name}>
+                    {repo.name}
+                </li>)}
+            </ul>
+            <pre>
+                {JSON.stringify(data, null, 3)}
+            </pre>
+        </div>
     )
 }
 
